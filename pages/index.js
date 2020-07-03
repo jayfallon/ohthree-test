c812c4ff7085b6b3cfda303e510484b77bd1bdff
Jay Fallon <jayfallon@gmail.com>
2020-07-03 16:17:54 -0400

    another commit

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,9 @@ export default function Home() {
 
       <main>
         <h1>Coming Soon-ish!</h1>
+        <section>
+          <h2>Please deploy</h2>
+        </section>
       </main>
 
       <footer>&copy;2020 - Oh! Three Banh Mi</footer>
